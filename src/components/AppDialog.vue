25ac8faacc1fabbed0527f2ad2c2097c814039ec
Ivan Pavlyuchenko <ozzmosis94@googlemail.com>
2022-04-11 08:13:30 +0300

    init

---
+++ src/components/AppDialog.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="primary" dark v-bind="attrs" v-on="on">
+        asd{{ buttonText }}
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">User Profile</span>
+      </v-card-title>
+      <v-card-text>
+        <slot />
+        <small>*indicates required field</small>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" text @click="dialog = false">
+          Close
+        </v-btn>
+        <v-btn color="blue darken-1" text @click="save"> Save </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+  props: {
+    buttonText: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    save() {
+      this.dialog = false;
+      this.$nextTick(() => this.$emit("save"));
+    },
+  },
+};
+</script>
+
+<style></style>
